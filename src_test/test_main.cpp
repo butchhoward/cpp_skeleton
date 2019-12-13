8827d1a88aaa5117d8a96d2d4d48ccb9d0057e98
Butch Howard <butch.howard@accenture.com>
2019-12-13 13:32:25 -0600

    update the skeleton from things learned in AoC2019

--- src_test/test_main.cpp
+++ src_test/test_main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include "test_runner.h"
+
+//MAKEMODULE INCLUDE MARKER. DO NOT DELETE
+
+
+bool hello_world_test()
+{
+    std::cout << "Hello World" << std::endl;
+    return std::cout.good();
+}
 
 int main()
 {
-    std::cout << "Hello Test World" << std::endl;
-    return 0;
+    test_runner::Tests tests = {
+        {"hello world test",      hello_world_test}
+        // ,{"XXX", XXX_test}
+        ,{"foo_test", foo_test}
+        ,{"foo_test", foo_test}
+        ,{"farkle_test", farkle_test}
+        ,{"frazzle_test", frazzle_test}
+        //MAKEMODULE LIST MARKER. DO NOT DELETE
+    };
+
+    return test_runner::run_tests(tests) ? 0 : 1;
+
+
 }
